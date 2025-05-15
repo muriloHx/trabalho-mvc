ea733b1ba6a7f7a7a4ecf58b6bab104535e4ea2f
Murilo <twitterfo131@gmail.com>
2025-05-15 14:49:42 -0300

    exemplo de design pattern Observer

---
+++ scripts/index.php
@@ -0,0 +1,67 @@
+<?php
+interface Investidor //padrao que todos os observers deverão seguir
+{
+    public function update(float $val);
+}
+
+class User implements Investidor
+{
+    private string $nome;
+
+    public function __construct(string $nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function update(float $val)
+    {
+        echo "Olá " . $this->nome . "O valor da sua ação é " . $val;
+    }
+}
+
+//subject
+class Acao
+{
+    private float $valor;
+    private $acionistas = [];
+
+    public function __construct(float $valAcaoInicial)
+    {
+        $this->valor = $valAcaoInicial;
+    }
+
+    public function alterarValor(float $val)
+    {
+        $this->valor = $val;
+        $this->notificar(); // notificará sempre o valor da acao mudar
+    }
+
+    public function remover(Investidor $inv)
+    {
+        $k = array_search($inv, $this->acionistas, true);
+        if ($k === true) {
+            unset($this->acionistas[$k]);
+        }
+    }
+    public function add(Investidor $inv)
+    {
+        $this->acionistas[] = $inv;
+    }
+
+    private function notificar()
+    {
+        foreach ($this->acionistas as $ac) {
+            $ac->update($this->valor);
+        }
+    }
+}
+
+$petrobras = new Acao(10000);
+
+$ac1 = new User("Joao");
+$ac2 = new User("Maria");
+
+$petrobras->add($ac1);
+$petrobras->add($ac2);
+
+$petrobras->alterarValor(90000);
